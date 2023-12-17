ecd4ad79374c95137d6710204111a2acd18b0191
Sito8943 <sito8943@gmail.com>
2023-12-17 14:36:01 -0800

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// app
+import App from "./App.jsx";
+
+// styles
+import "./index.css";
+import { ModeProvider, StyleProvider } from "@sito/ui";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ModeProvider>
+    <StyleProvider>
+      <App />
+    </StyleProvider>
+  </ModeProvider>
+);
