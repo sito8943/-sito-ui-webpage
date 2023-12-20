6bbad08822855637198c6dbb1ab281b91135aa8f
Sito8943 <sito8943@gmail.com>
2023-12-20 11:27:47 -0500

    chromatic done

--- src/views/Home/sections/Colors/sections/Chromatic.jsx
+++ src/views/Home/sections/Colors/sections/Chromatic.jsx
@@ -1,14 +1,15 @@
 import { useTranslation } from "react-i18next";
 
+// @emotion/css
+import { css } from "@emotion/css";
+
 // @sito/ui
 import { PrintAfter, useScreenHandler, useStyle } from "@sito/ui";
 
 // components
 import Card from "../../../../../components/Card/Card";
+import Logo from "../../../../../components/Logo/Logo";
 import RGBColor from "../components/RGBColor";
-
-// images
-import logo from "../../../../../assets/images/logo.svg";
 
 export default function Chromatic() {
   const { t } = useTranslation();
@@ -24,57 +25,78 @@ export default function Chromatic() {
       </PrintAfter>
       <div className="w-full grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-x-2 gap-y-4">
         <PrintAfter animation="appear" onVisible delay={400}>
-          <Card inverted className="relative flex items-center justify-center">
+          <Card
+            inverted
+            className={`relative flex items-center justify-center `}
+          >
             <div className="p-3 flex absolute w-full h-full flex-col justify-between items-start">
               <p className="uppercase">{colors.basics.text}</p>
               <RGBColor color={colors.basics.text} />
             </div>
-            <img src={logo} alt="logo" className="w-40 h-40" />
+            <Logo color={colors.basics.dark} className="w-40 h-40" />
           </Card>
         </PrintAfter>
         <PrintAfter animation="appear" onVisible delay={400}>
-          <Card inverted className="relative flex items-center justify-center">
+          <Card
+            inverted
+            className={`relative flex items-center justify-center ${css({
+              background: `#EDEDED !important`,
+            })}`}
+          >
             <div className="p-3 flex absolute w-full h-full flex-col justify-between items-start">
-              <p className="uppercase">{colors.basics.text}</p>
-              <RGBColor color={colors.basics.text} />
+              <p className="uppercase">#EDEDED</p>
+              <RGBColor color="#EDEDED" />
             </div>
-            <img src={logo} alt="logo" className="w-40 h-40" />
+            <Logo color={colors.basics.dark} className="w-40 h-40" />
           </Card>
         </PrintAfter>
         <PrintAfter animation="appear" onVisible delay={400}>
-          <Card inverted className="relative flex items-center justify-center">
+          <Card
+            inverted
+            className={`relative flex items-center justify-center ${css({
+              background: `#E1E1E1 !important`,
+            })}`}
+          >
             <div className="p-3 flex absolute w-full h-full flex-col justify-between items-start">
-              <p className="uppercase">{colors.basics.text}</p>
-              <RGBColor color={colors.basics.text} />
+              <p className="uppercase">#E1E1E1</p>
+              <RGBColor color="#E1E1E1" />
             </div>
-            <img src={logo} alt="logo" className="w-40 h-40" />
+            <Logo color={colors.basics.dark} className="w-40 h-40" />
           </Card>
         </PrintAfter>
         <PrintAfter animation="appear" onVisible delay={400}>
-          <Card inverted className="relative flex items-center justify-center">
+          <Card
+            className={`relative flex items-center justify-center ${css({
+              background: `${colors.basics.light} !important`,
+            })}`}
+          >
             <div className="p-3 flex absolute w-full h-full flex-col justify-between items-start">
-              <p className="uppercase">{colors.basics.text}</p>
-              <RGBColor color={colors.basics.text} />
+              <p className="uppercase">{colors.basics.light}</p>
+              <RGBColor color={colors.basics.light} />
             </div>
-            <img src={logo} alt="logo" className="w-40 h-40" />
+            <Logo color={colors.basics.text} className="w-40 h-40" />
           </Card>
         </PrintAfter>
         <PrintAfter animation="appear" onVisible delay={400}>
-          <Card inverted className="relative flex items-center justify-center">
+          <Card
+            className={`relative flex items-center justify-center ${css({
+              background: colors.basics.default,
+            })}`}
+          >
             <div className="p-3 flex absolute w-full h-full flex-col justify-between items-start">
-              <p className="uppercase">{colors.basics.text}</p>
-              <RGBColor color={colors.basics.text} />
+              <p className="uppercase">{colors.basics.default}</p>
+              <RGBColor color={colors.basics.default} />
             </div>
-            <img src={logo} alt="logo" className="w-40 h-40" />
+            <Logo color={colors.basics.text} className="w-40 h-40" />
           </Card>
         </PrintAfter>
         <PrintAfter animation="appear" onVisible delay={400}>
-          <Card inverted className="relative flex items-center justify-center">
+          <Card bordered className="relative flex items-center justify-center">
             <div className="p-3 flex absolute w-full h-full flex-col justify-between items-start">
-              <p className="uppercase">{colors.basics.text}</p>
-              <RGBColor color={colors.basics.text} />
+              <p className="uppercase">{colors.basics.dark}</p>
+              <RGBColor color={colors.basics.dark} />
             </div>
-            <img src={logo} alt="logo" className="w-40 h-40" />
+            <Logo color={colors.basics.text} className="w-40 h-40" />
           </Card>
         </PrintAfter>
       </div>
