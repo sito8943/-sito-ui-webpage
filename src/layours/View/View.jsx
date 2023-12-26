e9b3a4c2ded454014345a5dd75ce17120a8fb299
Sito8943 <sito8943@gmail.com>
2023-12-25 19:37:39 -0500

    doing navbar

--- src/layours/View/View.jsx
+++ src/layours/View/View.jsx
@@ -3,9 +3,13 @@ import { Outlet } from "react-router-dom";
 // @sito/ui
 import { ToTop } from "@sito/ui";
 
+// components
+import Navbar from "./components/Navbar";
+
 function View() {
   return (
     <>
+      <Navbar />
       <ToTop />
       <Outlet />
     </>
--- src/layours/View/components/Navbar.jsx
+++ src/layours/View/components/Navbar.jsx
@@ -1,11 +1,51 @@
+import { useState, useEffect, useCallback } from "react";
 import { useTranslation, withTranslation, Trans } from "react-i18next";
+import { useLocation } from "react-router-dom";
+
+// @sito/ui
+import { IconButton } from "@sito/ui";
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
+  const [transparency, setTransparency] = useState(true);
+
+  const onScroll = useCallback(() => {
+    const top = window.scrollY;
+    setTransparency(top < 60);
+  }, [setTransparency]);
+
+  useEffect(() => {
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [onScroll]);
+
   const { t, i18n } = useTranslation();
+
+  const location = useLocation();
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
 
-  return <div></div>;
+  return (
+    <header
+      className={`flex items-center justify-center w-full fixed top-0 left-0 min-h-[60px] transition duration-500 z-[50] ${
+        transparency ? "" : "bg-dark-drawer-background backdrop-blur-lg"
+      }`}
+    >
+      <div
+        className={`transition-all duration-500 flex items-center justify-between responsive-container ${
+          transparency ? "h-[100px]" : " h-[60px]"
+        }`}
+      >
+        <a></a>
+        <nav></nav>
+        <IconButton icon={faBars} />
+      </div>
+    </header>
+  );
 }
