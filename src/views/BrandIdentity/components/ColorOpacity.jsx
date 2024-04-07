bfd2b326fa9a6d54a7b0f0379cc8643ce834c8c0
Sito8943 <sito8943@gmail.com>
2024-04-07 12:38:22 -0400

    color opaicty

--- src/views/BrandIdentity/components/ColorOpacity.jsx
+++ src/views/BrandIdentity/components/ColorOpacity.jsx
@@ -7,28 +7,28 @@ function ColorOpacity(props) {
   return (
     <div className="flex h-full md:flex-col-reverse">
       <div
-        className={`h-full flex items-end justify-start p-3 pr-10 ${css({
+        className={`h-full md:h-auto flex items-end justify-start p-3 pr-10 ${css({
           background: `${color ?? colorHex}1E`,
         })}`}
       >
         <p className={`${css({ color: color ?? colorHex })} roboto-400`}>30%</p>
       </div>
       <div
-        className={`h-full flex items-end justify-start p-3 pr-10 ${css({
+        className={`h-full md:h-auto flex items-end justify-start p-3 pr-10 ${css({
           background: `${color ?? colorHex}32`,
         })}`}
       >
         <p className="text-white roboto-400">50%</p>
       </div>
       <div
-        className={`h-full flex items-end justify-start p-3 pr-10 ${css({
+        className={`h-full md:h-auto flex items-end justify-start p-3 pr-10 ${css({
           background: `${color ?? colorHex}50`,
         })}`}
       >
         <p className="text-white roboto-400">80%</p>
       </div>
       <div
-        className={`h-full flex items-end justify-start w-full p-3 ${css({
+        className={`h-full md:h-[200px] flex items-end md:items-start justify-start w-full p-3 ${css({
           background: `${color ?? colorHex}`,
         })}`}
       >
