5fcf7b8e2a40e21a8022014c9d59f08771007595
Sito8943 <sito8943@gmail.com>
2024-04-07 12:35:57 -0400

    navbar

--- src/layouts/Natural/components/Navbar.jsx
+++ src/layouts/Natural/components/Navbar.jsx
@@ -2,12 +2,18 @@ import { useState, useCallback, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
+// font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+// emotion
 import { css } from "@emotion/css";
+
+// @sito-ui
 import { IconButton } from "@sito/ui";
 
 // components
+import NavigationDrawer from "./NavigationDrawer";
 import SitoLogo from "../../../components/Logos/SitoLogo";
 
 const links = [
@@ -46,55 +52,62 @@ function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header
-      className={`expand flex items-center justify-center w-full fixed top-0 left-0 min-h-[60px] transition duration-500 z-[50] ${
-        transparency ? "" : "bg-bg backdrop-blur-lg"
-      }`}
-    >
-      <div
-        className={`relative transition-all duration-500 flex items-center justify-between ${
-          pathname === "/brand-identity" ? "px-20 sm:px-10" : "px-10"
-        } w-full ${transparency ? "h-[100px]" : "h-[80px]"}`}
+    <>
+      <header
+        className={`expand flex items-center justify-center w-full fixed top-0 left-0 min-h-[60px] transition duration-500 z-[50] ${
+          transparency ? "" : "bg-bg backdrop-blur-lg"
+        }`}
       >
-        <Link
-          to="/"
-          className={`text-white transition duration-500 ease-in-out ${
-            showLogo ? "opacity-100" : "opacity-0"
-          }`}
+        <div
+          className={`relative transition-all duration-500 flex items-center justify-between ${
+            pathname === "/brand-identity" ? "px-20 sm:px-10" : "px-10"
+          } w-full ${transparency ? "h-[100px]" : "h-[80px]"}`}
         >
-          <SitoLogo className="w-[100px] h-[75px]" />
-        </Link>
-        <nav className="sm:hidden">
-          <ul className="flex gap-10">
-            {links.map(({ href, id }) => (
-              <li
-                key={id}
-                className="flex flex-col items-center justify-center"
-              >
-                <a href={href} className={`text-white transition`}>
-                  {t(`_pages:routes.${id}`)}
-                </a>
-                <div
-                  className={`w-[120%] grid ${css({
-                    gridTemplateRows: pathname === href ? "1fr" : "0fr",
-                    transition: "grid-template-rows 0.5s ease-in-out",
-                  })}`}
+          <Link
+            to="/"
+            className={`text-white transition duration-500 ease-in-out ${
+              showLogo ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <SitoLogo className="w-[100px] h-[75px]" />
+          </Link>
+          <nav className="sm:hidden">
+            <ul className="flex gap-10">
+              {links.map(({ href, id }) => (
+                <li
+                  key={id}
+                  className="flex flex-col items-center justify-center"
                 >
-                  <div className="w-full overflow-hidden">
-                    <div className={`border-white border-[1px] h-0`}></div>
+                  <a href={href} className={`text-white transition`}>
+                    {t(`_pages:routes.${id}`)}
+                  </a>
+                  <div
+                    className={`w-[120%] grid ${css({
+                      gridTemplateRows: pathname === href ? "1fr" : "0fr",
+                      transition: "grid-template-rows 0.5s ease-in-out",
+                    })}`}
+                  >
+                    <div className="w-full overflow-hidden">
+                      <div className={`border-white border-[1px] h-0`}></div>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <IconButton
-          onClick={() => setShowMenu(true)}
-          icon={<FontAwesomeIcon icon={faBars} />}
-          className="!text-white msm:hidden absolute top-[1.3rem] right-10"
-        />
-      </div>
-    </header>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <IconButton
+            onClick={() => setShowMenu(true)}
+            icon={<FontAwesomeIcon icon={faBars} />}
+            className="!text-white msm:hidden absolute top-[1.3rem] right-10"
+          />
+        </div>
+      </header>
+      <NavigationDrawer
+        links={links}
+        visible={showMenu}
+        onClose={() => setShowMenu(false)}
+      />
+    </>
   );
 }
 
