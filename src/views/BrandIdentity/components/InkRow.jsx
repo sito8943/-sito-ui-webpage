9f6757401124d260c9a4a172fc08a430980153b6
Sito8943 <sito8943@gmail.com>
2024-04-09 21:11:17 -0400

    responvisve

--- src/views/BrandIdentity/components/InkRow.jsx
+++ src/views/BrandIdentity/components/InkRow.jsx
@@ -8,21 +8,21 @@ function InkRow(props) {
   const { fontColor, logoColor, sitoColor, color, bg } = props;
 
   return (
-    <div className="grid grid-cols-6 md:grid-cols-2 xs:grid-cols-1 gap-5 w-full items-center justify-items-center">
+    <div className="grid grid-cols-6 md:grid-cols-2 sm:grid-cols-1 gap-5 w-full items-center justify-items-center">
       <div className="md:hidden"></div>
       <div className="md:hidden"></div>
       <FullVerticalLogo
         fontColor={fontColor ?? color}
         logoColor={logoColor ?? color}
         sitoColor={sitoColor ?? color}
-        className={`w-[160px] h-[180px] md:justify-self-start xs:justify-self-center ${bg}`}
+        className={`w-[160px] h-[180px] md:justify-self-start sm:justify-self-center ${bg}`}
       />
       <div className="md:hidden"></div>
       <FullHorizontalLogo
         fontColor={fontColor ?? color}
         logoColor={logoColor ?? color}
         sitoColor={sitoColor ?? color}
-        className={`md:justify-self-end xs:justify-self-center w-[255px] h-[160px] ${bg}`}
+        className={`md:justify-self-end sm:justify-self-center w-[255px] h-[160px] ${bg}`}
       />
       <div className="md:hidden"></div>
     </div>
