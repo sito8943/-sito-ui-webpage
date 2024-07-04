83509cd06cfa378c12e0045699149b4e23d3b0ba
Sito8943 <sito8943@gmail.com>
2024-07-04 07:35:38 -0400

    fixed graphics rows

--- src/views/BrandIdentity/components/Graphics.jsx
+++ src/views/BrandIdentity/components/Graphics.jsx
@@ -11,28 +11,25 @@ function Graphics() {
   const { t } = useTranslation();
 
   return (
-    <div className="content viewport grid grid-cols-2 md:grid-cols-1 gap-5">
-      <div className="basic-column">
-        <h2 className="text-2xl">
-          {t(`_pages:brandIdentity.resources.title`)}
-        </h2>
-        <h4 className="text-xl poppins-700">
-          {t(`_pages:brandIdentity.resources.graphics.title`)}
-        </h4>
+    <div className="content viewport gap-5 flex flex-col">
+      <h2 className="text-2xl">{t(`_pages:brandIdentity.resources.title`)}</h2>
+      <h4 className="text-xl poppins-700">
+        {t(`_pages:brandIdentity.resources.graphics.title`)}
+      </h4>
+      <div className="grid grid-cols-2 md:grid-cols-1 gap-5">
         <p className="body">
           {t("_pages:brandIdentity.resources.graphics.body.paragraph1")}
         </p>
-        <p className="body">
-          {t("_pages:brandIdentity.resources.graphics.body.paragraph2")}
-        </p>
-      </div>
-      <div className="basic-column">
-        <div className="hidden-on-md h-[70px]"></div>
         <div className="basic-flex-wrap">
           {Array.from({ length: 96 }, () => 0).map((_, i) => (
             <Angular key={i} />
           ))}
         </div>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-1 gap-5">
+        <p className="body">
+          {t("_pages:brandIdentity.resources.graphics.body.paragraph2")}
+        </p>
         <div className="basic-flex-wrap">
           {Array.from({ length: 60 }, () => 0).map((_, i) => (
             <Fragment key={i} />
