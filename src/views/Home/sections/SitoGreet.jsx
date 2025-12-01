7988915cd59f0d39daff69c315a59ff0d5ccd71c
Sito <40501794+sito8943@users.noreply.github.com>
2025-12-01 16:35:26 +0100

    main sito greet section

---
+++ src/views/Home/sections/SitoGreet.jsx
@@ -0,0 +1,12 @@
+function SitoGreet() {
+  return (
+    <section
+      id="sito-greet"
+      className={`!p-0 w-viewport flex items-center justify-center flex-col gap-10 w-full transition duration-500 ease-in-out`}
+    >
+        
+    </section>
+  );
+}
+
+export default SitoGreet;
